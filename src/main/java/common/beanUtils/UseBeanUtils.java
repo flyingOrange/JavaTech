aab374d10f36382a0e88543daa1f782808eb2e48
flyingOrange <370686124@qq.com>
2018-02-27 17:40:08 +0800

    加密相关

--- src/main/java/common/beanUtils/UseBeanUtils.java
+++ src/main/java/common/beanUtils/UseBeanUtils.java
@@ -2,6 +2,8 @@ package common.beanUtils;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.commons.beanutils.BeanUtils;
 
@@ -24,9 +26,20 @@ public class UseBeanUtils {
 		System.out.println("name="+name+",age="+age+",birth="+birth);
 		
 		//属性拷贝
-		Student student3 = new Student();
-		
-		
+		Student student3 = new Student("李四",18,new Date());
+		Student student4 = new Student();
+        BeanUtils.copyProperties(student4, student3);
+        System.out.println(student4);
+        
+        //把一个map集合中的数据拷贝到javaBean中
+        Map<String,Object> map = new HashMap<String,Object>();
+        map.put("name", "orange");
+        map.put("age", 100);
+        map.put("birth", new Date());
+
+        Student student5 = new Student();
+        BeanUtils.copyProperties(student5, map);
+        System.out.println(student5);
 		
 
 	}
