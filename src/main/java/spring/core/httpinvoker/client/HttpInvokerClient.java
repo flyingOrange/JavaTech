50b6b92600d63ed970548167837b8bc108f91e5b
flyingOrange <370686124@qq.com>
2018-09-10 20:09:44 +0800

    httpinvoker使用

--- src/main/java/spring/core/httpinvoker/client/HttpInvokerClient.java
+++ src/main/java/spring/core/httpinvoker/client/HttpInvokerClient.java
@@ -8,15 +8,11 @@ import spring.core.httpinvoker.server.service.UserService;
 
 public class HttpInvokerClient {
 	public static void main(String[] args) {
-
 		ApplicationContext ac = new ClassPathXmlApplicationContext(
-
 		"classpath:/spring/core/httpinvoker/client/client-context.xml");
-
+		
 		UserService service = (UserService) ac.getBean("userService");
-
 		User u = service.getUserbyId(2222);
-
 		System.out.println(u);
 
 	}
