f5c91d334c6d8838d8aaa23b01575a3905f5b86e
flyingOrange <370686124@qq.com>
2018-10-16 19:05:29 +0800

    加入protobuf

--- src/main/java/test/TestSomething.java
+++ src/main/java/test/TestSomething.java
@@ -1,6 +1,7 @@
 package test;
 
 import java.math.BigDecimal;
+import java.util.Date;
 
 import org.junit.Test;
 
@@ -9,16 +10,16 @@ public class TestSomething {
 	@Test
 	public void readFileNames() {
 		
-		BigDecimal total = BigDecimal.ZERO;
+		BigDecimal num3 = BigDecimal.TEN;
 		
-		BigDecimal num1 = BigDecimal.TEN;
+		BigDecimal num1 = BigDecimal.ZERO;
 		BigDecimal num2 = BigDecimal.ONE;
+		BigDecimal num4 = BigDecimal.ONE;
 		
-		total = total.add(num1);
-		total = total.add(num2);
 		
-		System.out.println(total);
-        
+		BigDecimal total = num1.add(num2).add(num4);
+		
+
         
 	}
 
