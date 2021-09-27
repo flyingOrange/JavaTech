be219b01ae2870a0bce567fcb6f11804f565f7e2
flyingOrange <370686124@qq.com>
2021-09-27 10:44:37 +0800

    bytebuf用法

--- src/main/java/netty/netty/buf/ByteBuf01.java
+++ src/main/java/netty/netty/buf/ByteBuf01.java
@@ -1,11 +1,25 @@
 package netty.netty.buf;
 
 import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufAllocator;
 import io.netty.buffer.Unpooled;
 
 public class ByteBuf01 {
 
 	public static void main(String[] args) {
+		//ByteBuf创建
+		ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer();
+		System.out.println(byteBuf);
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0;i<300;i++){
+			sb.append("a");
+		}
+		byteBuf.writeBytes(sb.toString().getBytes());
+		System.out.println(byteBuf);
+		//组成：可读、可写、容量、最大容量
+
+
+
 
 		
 		//下例包含了 byte[10]数组
@@ -18,14 +32,14 @@ public class ByteBuf01 {
 		System.out.println("capacity="+ buffer.capacity());
 		
 		//getByte不会移动readIndex,readableBytes才会移动readIndex
-		for(int i=0;i<buffer.capacity();i++) {
-			System.out.println(buffer.getByte(i));
-			
-		}
-		for(int i=0;i<buffer.capacity();i++) {
-			System.out.println(buffer.readableBytes());
-			
-		}
+//		for(int i=0;i<buffer.capacity();i++) {
+//			System.out.println(buffer.getByte(i));
+//
+//		}
+//		for(int i=0;i<buffer.capacity();i++) {
+//			System.out.println(buffer.readableBytes());
+//
+//		}
 		
 		
 	}
--- src/main/java/netty/netty/buf/ByteBuf02.java
+++ src/main/java/netty/netty/buf/ByteBuf02.java
@@ -1,37 +1,42 @@
 package netty.netty.buf;
 
-import java.nio.charset.Charset;
-
 import io.netty.buffer.ByteBuf;
-import io.netty.buffer.Unpooled;
+import io.netty.buffer.ByteBufAllocator;
+import io.netty.buffer.CompositeByteBuf;
 
+//ByteBuf零拷贝
 public class ByteBuf02 {
 
 	public static void main(String[] args) {
-		ByteBuf buffer = Unpooled.copiedBuffer("hello world!",Charset.forName("UTF-8"));
-		
-		if(buffer.hasArray()) {
-			byte[] content = buffer.array();
-			//将数据转化为字符串
-			System.out.println(new String(content,Charset.forName("UTF-8")));
-			
-			System.out.println("bytebuf="+buffer);
-			
-			System.out.println(buffer.arrayOffset());
-			System.out.println(buffer.readerIndex());
-			System.out.println(buffer.writerIndex());
-			System.out.println(buffer.capacity());
-			
-			int len = buffer.readableBytes();
-			System.out.println("len="+len);
-			
-			//按照某个范围读取
-			System.out.println(buffer.getCharSequence(0, 4, Charset.forName("UTF-8")));
-			System.out.println(buffer.getCharSequence(2, 4, Charset.forName("UTF-8")));
-			
-			
-		}
-		
+		ByteBuf byteBuf = ByteBufAllocator.DEFAULT.buffer(10);
+		byteBuf.writeBytes(new byte[]{'a','b','c','d','e','f','g','h','i','j'});
+		System.out.println(byteBuf);
+
+		//slice切片并没有复制数据,和原数据公用内存
+		ByteBuf f1= byteBuf.slice(0,5);
+		ByteBuf f2= byteBuf.slice(5,5);
+		f1.retain();//引用计数+1
+		byteBuf.release();//byteBuf引用计数-1，计数为0后f1 f2也不能用了
+		System.out.println(f1);
+		System.out.println(f2);
+		//duplicate和原数据公用一片内存，且不截取
+		ByteBuf f3 = byteBuf.duplicate();
+		//copy，和原数据不公用内存，再无关系
+		ByteBuf f4 = byteBuf.copy();
+
+		//多个ButeBuf聚合
+		ByteBuf bf1 = ByteBufAllocator.DEFAULT.buffer();
+		bf1.writeBytes(new byte[]{1,2,3,4,5});
+		ByteBuf bf2 = ByteBufAllocator.DEFAULT.buffer();
+		bf2.writeBytes(new byte[]{6,7,8,9,10});
+		ByteBuf bf = ByteBufAllocator.DEFAULT.buffer();
+		bf.writeBytes(bf1).writeBytes(bf2);//会发生数据拷贝
+		System.out.println(bf);
+
+		CompositeByteBuf buf = ByteBufAllocator.DEFAULT.compositeBuffer();//不会进行内存数据拷贝
+		buf.addComponents(true, bf1, bf2);
+		System.out.println(buf);
+
 	}
 	
 }
